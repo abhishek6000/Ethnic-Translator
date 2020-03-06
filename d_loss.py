c68ef0c060a93c7505805d0979f966e806670bbb
Abhishek Samanta <abhisheksamanta60@gmail.com>
2020-03-07 02:52:57 +0530

    discriminator loss

---
+++ d_loss.py
@@ -0,0 +1,5 @@
+def discriminator_loss(real, generated):
+  real_loss = loss_obj(tf.ones_like(real), real)
+  generated_loss = loss_obj(tf.zeros_like(generated), generated)
+  total_disc_loss = real_loss + generated_loss
+  return total_disc_loss * 0.5
